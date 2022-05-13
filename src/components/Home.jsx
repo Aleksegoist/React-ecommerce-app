4c603b68e7b1d04dc69659cb5c6dfb195e41c0c1
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-13 13:09:59 +0300

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,19 @@ import bg from '../img/bg/bg.jpg';
 const Home = () => {
     return (
         <div className='hero'>
-            <div class='card bg-dark text-white border-0'>
+            <div className='card bg-dark text-white border-0'>
                 <img
                     src={bg}
-                    class='card-img'
+                    className='card-img'
                     alt='background'
                     // height='550px'
                 />
-                <div class='card-img-overlay d-flex flex-column justify-content-center'>
+                <div className='card-img-overlay d-flex flex-column justify-content-center'>
                     <div className='container'>
-                        <h5 class='card-title display-3 fw-bolder mb-0'>
+                        <h5 className='card-title display-3 fw-bolder mb-0'>
                             NEW SEASON
                         </h5>
-                        <p class='card-text lead fs-2'>
+                        <p className='card-text lead fs-2'>
                             CHECK OUT ALL THE TRENDS
                         </p>
                     </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 
 const Products = () => {
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState([data]);
+    const [loading, setLoading] = useState([false]);
+    let componentMounted = true;
+
+    useEffect(() => {
+        const getProducts = async () => {
+            setLoading(true);
+            const response = await fetch('https://fakestoreapi.com/products');
+        };
+        setProducts();
+        if (componentMounted) {
+            setData(await response.clone().json()); 
+            setFilter(await response.json())
+            setLoading(false)
+            console.log(filter);
+        }
+        return () => {
+            componentMounted = false;
+        }
+    }, []);
+
     return <div>Products</div>;
 };
 
