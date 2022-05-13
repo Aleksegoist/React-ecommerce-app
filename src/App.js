fdcf7410c1675d4117841a53c8fa9c418cba3699
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-13 14:46:04 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Products from './components/Products';
 
 function App() {
     return (
         <>
             <Navbar />
-            <Home />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/products' element={<Products />} />
+            </Routes>
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Router>
         <App />
-    </React.StrictMode>
+    </Router>
 );
