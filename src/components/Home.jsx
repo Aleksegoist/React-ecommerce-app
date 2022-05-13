c708fcd40604d148dbc9ec87a07b77ac3e2b60e4
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-13 13:18:06 +0300

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import bg from '../img/bg/bg.jpg';
+import Products from './Products';
 
 const Home = () => {
     return (
@@ -22,6 +23,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <Products />
         </div>
     );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,20 +10,30 @@ const Products = () => {
         const getProducts = async () => {
             setLoading(true);
             const response = await fetch('https://fakestoreapi.com/products');
+            if (componentMounted) {
+                setData(await response.clone().json());
+                setFilter(await response.json());
+                setLoading(false);
+                console.log(filter);
+            }
+            return () => {
+                componentMounted = false;
+            };
         };
-        setProducts();
-        if (componentMounted) {
-            setData(await response.clone().json()); 
-            setFilter(await response.json())
-            setLoading(false)
-            console.log(filter);
-        }
-        return () => {
-            componentMounted = false;
-        }
+        getProducts();
     }, []);
 
-    return <div>Products</div>;
+    return (
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <h1>Latest Products</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Products;
