4c2a52b6099adb195a30f8f6d77cf9d47484670a
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-13 16:20:36 +0300

    add

---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Product = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState([]);
+
+    return <div>Product</div>;
+};
+
+export default Product;
